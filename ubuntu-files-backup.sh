8b4556f68025db8b61394bb3450705c5e99626c5
James Miskiewicz <james.miskiewicz@gmail.com>
2010-05-24 16:12:17 -0400

    ubuntu-files is a package backup list, add sources.list though

---
+++ ubuntu-files-backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# http://bit.ly/bynyi3
+# This will generate a list of installed packages on the current system
+# To reinstall a system from the file generated from this backup:
+#   sudo apt-get update
+#   sudo apt-get dist-upgrade
+#   dpkg -–set-selections < ubuntu-files
+#   sudo dselect
+sleep 10s;dpkg --get-selections | grep -v deinstall > /home/james/ubuntu-files
